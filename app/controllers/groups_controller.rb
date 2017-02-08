445f3998dc9f8edf3cd8e86c0b0b02cefbd0e1ec
nathanye <nathanydm@hotmail.com>
2017-02-08 11:25:52 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,45 +1,71 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-  def index
-    @groups = Group.all
-  end
-
-  def show
-    @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
-  end
-
-  def edit
-  end
-
-  def new
-   @group = Group.new
-  end
-
-  def create
-    @group = Group.new(group_params)
-    @group.user = current_user
-
-    if @group.save
-      redirect_to groups_path
-    else
-       render :new
+    def index
+      @groups = Group.all
     end
-  end
 
-  def update
-    if @group.update(group_params)
-      redirect_to groups_path, notice: "Update Success"
-    else
-      render :edit
+    def show
+      @group = Group.find(params[:id])
+      @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
     end
-  end
 
-  def destroy
-    @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
-  end
+    def edit
+    end
+
+    def new
+     @group = Group.new
+    end
+
+    def create
+      @group = Group.new(group_params)
+      @group.user = current_user
+
+      if @group.save
+        redirect_to groups_path
+      else
+         render :new
+      end
+    end
+
+    def update
+      if @group.update(group_params)
+        redirect_to groups_path, notice: "Update Success"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @group.destroy
+      redirect_to groups_path, alert: "Group deleted"
+    end
+
+    def join
+     @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "你已经是本讨论版成员了！"
+    end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出本讨论版！"
+      else
+        flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+      redirect_to group_path(@group)
+    end
 
  private
 
